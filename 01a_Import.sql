d502cf7f5e9e4cae730e97dd54e0e8ded8efa66f
skparkes <bias.variance@gmail.com>
2015-03-21 12:54:20 -0400

    Ram the data into SQLite.
    Do this to have a sane data storage medium.  Got it close enough to be
    useful.

---
+++ 01a_Import.sql
@@ -0,0 +1,18 @@
+﻿.separator "≡"
+.echo ON
+.timer ON
+
+pragma synchronous = OFF;
+pragma page_size = 4096;
+pragma cache_size = 100000;
+
+create table blogs (document TEXT);
+.import "en_US.blogs.txt" blogs
+
+create table news (document TEXT);
+.import "en_US.news.txt" news
+
+create table twitter (document TEXT);
+.import "en_US.twitter.txt" twitter
+
+.exit
---
+++ 01b_Import.bat
@@ -0,0 +1,1 @@
+sqlite3.exe en_US.sqlite < 01a_Import.sql
