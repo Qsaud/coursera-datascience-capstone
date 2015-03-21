14519b5448471f9c9b5bc1f7773e12e6fcc7ee65
skparkes <bias.variance@gmail.com>
2015-03-21 14:33:39 -0400

    Refactor out the annoying batch script.
    Do this to keep the language overhead to a minimum.

--- 01b_Create_DB.bat
+++
@@ -1,2 +0,0 @@
-del en_US.sqlite
-sqlite3.exe en_US.sqlite < 01a_Create_DB.sql
--- 01b_SQLite_Load.R
+++ 01b_SQLite_Load.R
@@ -7,25 +7,46 @@ assign('length', sql_prefix('length', 1), envir=base_agg)
 assign('glob', sql_prefix('glob', 2), envir=base_agg)
 assign('like', sql_prefix('like', 2), envir=base_agg)
 
+
 dir.repo <- paste0(Sys.getenv('PathGitHubRepos'), '/coursera-datascience-capstone/') %T>% print()
 setwd(dir.repo)
+
+name.db <- 'en_US.sqlite'
+if(file.exists(name.db)){file.remove(name.db)}
+system2('sqlite3.exe', name.db, stdin = '01a_Create_DB.sql')
 
 db_en_us <- src_sqlite('en_US.sqlite') %T>% print() %T>% str()
 names.tbls <- src_tbls(db_en_us) %>% setNames(., .) %T>% print()
 
 BatchImport <- function(name.table, nrow.cache=10000) {
-  con.text <- paste0("en_US.", name.table, ".txt") %>% file("r")
-  cache <- con.text %>% readLines(nrow.cache)
+  
+  FetchChunk <- . %>%
+    readLines(
+      n = nrow.cache
+      ,skipNul = TRUE
+    )
+  
+  con.text <- paste0(
+    "en_US."
+    ,name.table
+    ,".txt"
+  ) %T>%
+    print() %>%
+    file("r")
+  
+  cache <- con.text %>% FetchChunk()
   while(length(cache)) {
     dbWriteTable(
       db_en_us$con
       ,name.table
       ,value = data.frame(document=cache)
       ,append = TRUE
-      )
-    cache <- con.text %>% readLines(nrow.cache)
+    )
+    cache <- con.text %>% FetchChunk()
   }
+  
   con.text %>% close()
+  
   return(tbl(db_en_us, name.table))
 }
 
